d9b7db81b5b54bfe75eb9ef1d4609061ca2f7cb0
Mateusz Czaplinski <czapkofan@gmail.com>
2016-06-30 18:07:55 +0000

    iterate NULL-delimited list

--- homer.sh
+++ homer.sh
@@ -1,2 +1,11 @@
 #!/usr/bin/env bash
 
+src=./dira
+dst=./dirb
+
+find "$src/" -type f -print0 |
+        while IFS= read -r -d '' file; do
+                printf '%s\n' "$file"
+        done
+
+# TODO: create links
