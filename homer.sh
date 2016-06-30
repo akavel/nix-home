3bf57e6c2d5552570ba02e53215f159e16033c40
Mateusz Czaplinski <czapkofan@gmail.com>
2016-06-30 18:35:28 +0000

    create links

--- homer.sh
+++ homer.sh
@@ -5,9 +5,12 @@ set -o pipefail
 src=./dira
 dst=./dirb
 
+function die { echo "$(basename "$0"): $@" >&2; exit 1; }
+
 find "$src/" -type f -printf '%P\0' |
-    while IFS= read -r -d '' file; do
-        printf '%s\n' "$file"
+    while IFS= read -r -d '' path; do
+        mkdir -p "$(dirname "$dst/$path")"
+        # Create link if target doesn't exist (this is default behavior of ln)
+        ln -s -r "$src/$path" "$dst/$path"
     done
 
-# TODO: create links
