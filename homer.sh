c198e7b3a5b094e94d8da9c1f356e779f597d686
Mateusz Czaplinski <czapkofan@gmail.com>
2016-06-30 19:18:11 +0000

    first sketch of code for detecting added/removed files

--- homer.sh
+++ homer.sh
@@ -5,7 +5,28 @@ set -o pipefail
 src=./dira
 dst=./dirb
 
-function die { echo "$(basename "$0"): $@" >&2; exit 1; }
+function die {
+    echo "$(basename "$0"): $@" >&2
+    exit 1
+}
+function subtree {
+    local root="$1"
+    find "$root/" -type f -printf '%P\0' |
+        sort -z
+}
+
+# Create new files
+comm -z -13 <( cat lista | sort -z ) \
+            <( subtree "$src" ) |
+    while IFS= read -r -d '' path; do
+        echo "added: [$path]"
+    done
+comm -z -23 <( cat lista | sort -z ) \
+            <( subtree "$src" ) |
+    while IFS= read -r -d '' path; do
+        echo "removed: [$path]"
+    done
+
 
 find "$src/" -type f -printf '%P\0' |
     while IFS= read -r -d '' path; do
